a28bb017b17560ce7bc09e0882bb966d71fb6594
boon4681 <boon4681dev@gmail.com>
2025-07-25 04:09:09 +0700

    update

---
+++ src/serverwofacade/ServerProvider.java
@@ -0,0 +1,27 @@
+package serverwofacade;
+
+public class ServerProvider {
+    private ScheduleServer scheduleServer = null;
+
+    public ServerProvider() {
+        scheduleServer = new ScheduleServer();
+    }
+
+    public void startServer() {
+        scheduleServer.startBooting();
+        scheduleServer.readSystemConfigFile();
+        scheduleServer.init();
+        scheduleServer.initializeContext();
+        scheduleServer.initializeListeners();
+        scheduleServer.createSystemObjects();
+    }
+
+    public void greatFullShutdown() {
+        scheduleServer.releaseProcesses();
+        scheduleServer.destory();
+        scheduleServer.destroySystemObjects();
+        scheduleServer.destoryListeners();
+        scheduleServer.destoryContext();
+        scheduleServer.shutdown();
+    }
+}
